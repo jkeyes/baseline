17220f248269299f6e262eeb3854e217517b6f33
John Keyes <johnkeyes@gmail.com>
2011-11-08 22:44:18 +0000

    Try to ensure the load handler only runs to completion the first time it is invoked.

--- baseliner/bookmarklet.src.js
+++ baseliner/bookmarklet.src.js
@@ -3,15 +3,23 @@ javascript:(function(){
   if (window.baseliner) {
     baseliner.toggle();
   } else {
+    var _already_loaded = document.getElementById('baseliner-script');
+    if (_already_loaded) {
+      return;
+    };
     var _baseliner=document.createElement('script');
+    _baseliner.id='baseliner-script';
     _baseliner.type='text/javascript';
-    _baseliner.src='https://github.com/downloads/jkeyes/baseline/baseliner-latest.min.js';
-    document.getElementsByTagName('head')[0].appendChild(_baseliner);
-    var loadFunction = function() {
+    _baseliner.src='http://files.keyes.ie/baseliner-latest.js';
+    document.getElementsByTagName('body')[0].appendChild(_baseliner);
+    var loadFunction = function(evt) {
+      if (window.baseliner) {
+        return;
+      };
       baseliner = new Baseliner(_grid_height);
       baseliner.toggle();
     };
-    _baseliner.onload = loadFunction;
     _baseliner.onreadystatechange = loadFunction;
-  }
+    _baseliner.onload = loadFunction;
+  };
 })();
